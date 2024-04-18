82fca30fe7f6b6cee6dec7494b21599fa9c789b6
Nikolov <Hristo.Nikolov@Honeywell.com>
2024-04-18 17:05:51 +0200

    Sorting the files with the presets before initializing the patches. The order of reading the files in a directory on the SD card is not guaranteed to be always the same. Therefore, first read and sort the files based on their names. The original load function is still available.

--- thr_ii/init_patches.cpp
+++ thr_ii/init_patches.cpp
@@ -42,6 +42,9 @@ std::string readFile(File file)
   return str;
 }
 
+//-----------------
+// Not used anymore
+//-----------------
 void initializePresets(File dir, std::vector <JsonDocument> &json_patchesII, std::vector <String> &patchNames, uint16_t &npatches)
 {
   JsonDocument doc;
@@ -79,6 +82,64 @@ void initializePresets(File dir, std::vector <JsonDocument> &json_patchesII, std
   npatches = json_patchesII.size();
 }
 
+//------------------------------------------------------
+// The order of reading the files in a directory on the SD card is not guaranteed to be always the same
+// Therefore, first read and sort the files based on their names. Use file name format: "01 name etc.thrl6p"
+//------------------------------------------------------
+void initializeSortedPresets(const String &path, std::vector <JsonDocument> &json_patchesII, std::vector <String> &patchNames, uint16_t &npatches)
+{
+  JsonDocument doc;
+//  std::vector <File> directory; // Uses more memory
+  std::vector <String> file_paths;
+
+  File dir = SD.open(path.c_str());
+
+  while( true )
+  {
+    File entry = dir.openNextFile();
+    if( !entry ) { break; } // No more files
+    if( entry.available() ) // Zero is returned for directories
+    {
+      String fpath = path + "/" + String(entry.name());
+      Serial.println( fpath );
+//      directory.push_back(entry);
+      file_paths.push_back(fpath);
+    }
+    entry.close(); // DO NOT CLOSE IF PUSH File objects!!!
+  }
+
+//  if( directory.size() > 0 )
+  if( file_paths.size() > 0 )
+  {
+//    std::sort(directory.begin(), directory.end(), [](File a, File b) { return strcasecmp(a.name(), b.name()) < 0; });
+    std::sort(file_paths.begin(), file_paths.end(), [](String a, String b) { return strcasecmp(a.c_str(), b.c_str()) < 0; });
+
+    for(auto fpath : file_paths)
+//    for(auto entry : directory)
+    {
+      //Serial.println(fpath);
+      File entry = SD.open(fpath.c_str(), FILE_READ);
+      std::string data = readFile( entry );
+//      Serial.println( data.c_str() );
+      entry.close();
+
+      DeserializationError dse = deserializeJson(doc, data);
+      if( dse )
+      {
+          Serial.print("deserializeJson() failed: ");
+          Serial.println( dse.c_str() );
+          continue;
+      }
+      
+      json_patchesII.push_back( doc );
+      patchNames.push_back((const char*) (doc["data"]["meta"]["name"]));            
+      Serial.println((const char*) (doc["data"]["meta"]["name"])); 
+    }
+    npatches = json_patchesII.size();
+  }
+}
+//------------------------------------------------------
+
 void init_patches_from_sdcard()
 {
   if (!SD.begin(sd_chipsel))
@@ -91,14 +152,16 @@ void init_patches_from_sdcard()
     TRACE_THR30IIPEDAL(Serial.print(F("VolumeBegin -> success. FAT-Type: "));)
 
     Serial.println("Initializing user presets from SDCard");
-    File dir = SD.open(path_presets_user);
-    dir.seek(0); // Always start from the first file in the directory?
-    initializePresets(dir, json_patchesII_user, libraryPatchNames, npatches_user);
+//    File dir = SD.open(path_presets_user);
+//    dir.seek(0); // Always start from the first file in the directory?
+//    initializePresets(dir, json_patchesII_user, libraryPatchNames, npatches_user);
+    initializeSortedPresets(path_presets_user, json_patchesII_user, libraryPatchNames, npatches_user);
 
     Serial.println("Initializing factory presets from SDCard");
-    dir = SD.open(path_presets_factory);
-    dir.seek(0); // Always start from the first file in the directory?
-    initializePresets(dir, json_patchesII_factory, factoryPatchNames, npatches_factory);
+//    dir = SD.open(path_presets_factory);
+//    dir.seek(0); // Always start from the first file in the directory?
+//    initializePresets(dir, json_patchesII_factory, factoryPatchNames, npatches_factory);
+    initializeSortedPresets(path_presets_factory, json_patchesII_factory, factoryPatchNames, npatches_factory);
 
     TRACE_THR30IIPEDAL(Serial.printf(F("\n\rLoaded %d JSON user patches and %d JSON factory patches.\n\r"), npatches_user, npatches_factory);)
   }
